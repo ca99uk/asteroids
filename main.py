7f10c3508184d1a5c8001cd03216d237d1eca9f9
Colin <ca99uk@gmail.com>
2026-04-13 16:59:37 +0100

    Added kill for asteroids

--- main.py
+++ main.py
@@ -48,6 +48,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if roid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    roid.kill()
+                    shot.kill()
         for draw in drawable:
             draw.draw(screen)
         pygame.display.flip()
